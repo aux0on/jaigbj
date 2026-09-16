956619734d759f1b61d6182d2c3ea74c114af147
unleaked <lzzxhq@gmail.com>
2026-09-16 11:21:15 -0700

    Add asynchronous loading of Prevention.lua
    Load Prevention.lua from GitHub asynchronously.

--- .lua
+++ .lua
@@ -53,6 +53,9 @@ end
 local RootMaid = Maid.new()
 
 local shared = odh_shared_plugins
+task.spawn(function()
+    shared.load_from_github_url("/aux0on/CrashHandler/refs/heads/main/Prevention.lua")
+end)
 
 local Services = {
     Players = game:GetService("Players"),
